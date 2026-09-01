eae24958d009554fac64667601b802aec53777d3
Daniele Trainini <dani84bs@gmail.com>
2026-09-01 13:27:51 +0000

    chore: bump release

--- _meta.lua
+++ _meta.lua
@@ -2,5 +2,5 @@ local _ = require("gettext")
 return {
     fullname = _("Remote Library"),
     description = _([[A plugin to access remote libraries]]),
-    version = "0.1.0",
+    version = "0.2.0",
 }
