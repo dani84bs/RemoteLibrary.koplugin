e69bb080fee8206ce0af9629265e137bc094af4e
Daniele Trainini <dani84bs@gmail.com>
2026-09-01 15:15:36 +0000

    fix(meta): fix version syntax

--- _meta.lua
+++ _meta.lua
@@ -2,5 +2,5 @@ local _ = require("gettext")
 return {
     fullname = _("Remote Library"),
     description = _([[A plugin to access remote libraries]]),
-    version = "0.2.0",
+    version = "v0.2.0",
 }
